ade13128ead31ab7b3a6f2105dc02774d70fdf6a
Wellington Donato <dev.donatello3@gmail.com>
2023-12-29 04:36:18 -0300

    Finalização do sistema Estacionamento.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,48 +1,77 @@
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
+
+
         private List<string> veiculos = new List<string>();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
             this.precoInicial = precoInicial;
             this.precoPorHora = precoPorHora;
+
         }
+        public string carro;
+
+        public string Veiculo
+        {
+            get => Console.ReadLine();
+
+            set
+            {
+                if (value.Length >= 8)
+                {
+                    carro = value;
+                }
+                else if (value != "")
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Veiculo = Console.ReadLine();
+            veiculos.Add(carro.ToUpper());
+
         }
+
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                int horas = Convert.ToInt32(Console.ReadLine());
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                veiculos.Remove(placa.ToUpper());
+
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+
             }
+
             else
             {
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
@@ -55,8 +84,12 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach (var veiculos in veiculos)
+                {
+                    Console.WriteLine(veiculos.ToString());
+
+                }
+
             }
             else
             {
--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,6 +6,8 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
+
+
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
@@ -13,8 +15,14 @@ precoInicial = Convert.ToDecimal(Console.ReadLine());
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
+try
+{
+    
+
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+
 
 string opcao = string.Empty;
 bool exibirMenu = true;
@@ -32,6 +40,7 @@ while (exibirMenu)
     switch (Console.ReadLine())
     {
         case "1":
+       
             es.AdicionarVeiculo();
             break;
 
@@ -57,3 +66,10 @@ while (exibirMenu)
 }
 
 Console.WriteLine("O programa se encerrou");
+
+}
+catch (Exception)
+{
+    
+    Console.WriteLine("Placa inválida, Ex: (XXX-XXXX)");
+}
